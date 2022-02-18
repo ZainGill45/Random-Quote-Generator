6aba64e45898051b60f8b2f5b15795a4233b7678
Zain <zainworking45@gmail.com>
2022-02-17 20:28:57 -0700

    Added color array appearance so is better

--- js/main.js
+++ js/main.js
@@ -99,6 +99,24 @@ const quotes = [
   ["Vitality shows not only in the ability to persist but in the ability to start over.", "F. Scott Fitzgerald", "Vitality"],
   ["The most common way people give up their power is by thinking they don’t have any.", "Alice Walker", "Power"],
 ]
+
+const randomColors = 
+[
+  '#e9ecef', '#dee2e6', '#ced4da', '#adb5bd', '#868e96', '#495057', '#343a40', '#212529', // Grey lighter variants not used '#f8f9fa', '#f1f3f5', 
+  '#ffc9c9', '#ffa8a8', '#ff8787', '#ff6b6b', '#fa5252', '#f03e3e', '#e03131', '#c92a2a', // Red lighter variants not used '#fff5f5', '#ffe3e3', 
+  '#fcc2d7', '#faa2c1', '#f783ac', '#f06595', '#e64980', '#d6336c', '#c2255c', '#a61e4d', //  Pink lighter variants not used '#fff0f6', '#ffdeeb', 
+  '#eebefa', '#e599f7', '#da77f2', '#cc5de8', '#be4bdb', '#ae3ec9', '#9c36b5', '#862e9c', // Grape lighter variants not used '#f8f0fc', '#f3d9fa', 
+  '#d0bfff', '#b197fc', '#9775fa', '#845ef7', '#7950f2', '#7048e8', '#6741d9', '#5f3dc4', // Violet lighter variants not used '#f3f0ff', '#e5dbff', 
+  '#bac8ff', '#91a7ff', '#748ffc', '#5c7cfa', '#4c6ef5', '#4263eb', '#3b5bdb', '#364fc7', // Indigo lighter variants not used '#edf2ff', '#dbe4ff', 
+  '#a5d8ff', '#74c0fc', '#4dabf7', '#339af0', '#228be6', '#1c7ed6', '#1971c2', '#1864ab', // Blue lighter variants not used '#e7f5ff', '#d0ebff', 
+  '#99e9f2', '#66d9e8', '#3bc9db', '#22b8cf', '#15aabf', '#1098ad', '#0c8599', '#0b7285', // Cyan lighter variants not used '#e3fafc', '#c5f6fa', 
+  '#96f2d7', '#63e6be', '#38d9a9', '#20c997', '#12b886', '#0ca678', '#099268', '#087f5b', // Teal lighter variants not used '#e6fcf5', '#c3fae8', 
+  '#b2f2bb', '#8ce99a', '#69db7c', '#51cf66', '#40c057', '#37b24d', '#2f9e44', '#2b8a3e', // Green lighter variants not used '#ebfbee', '#d3f9d8', 
+  '#d8f5a2', '#c0eb75', '#a9e34b', '#94d82d', '#82c91e', '#74b816', '#66a80f', '#5c940d', // Lime lighter variants not used '#f4fce3', '#e9fac8', 
+  '#ffec99', '#ffe066', '#ffd43b', '#fcc419', '#fab005', '#f59f00', '#f08c00', '#e67700', // Yellow lighter variants not used '#fff9db', '#fff3bf', 
+  '#ffd8a8', '#ffc078', '#ffa94d', '#ff922b', '#fd7e14', '#f76707', '#e8590c', '#d9480f', // Orange lighter variants not used '#fff4e6', '#ffe8cc', 
+];
+
 let viewedQuotes = [];
 
 const GenerateNewQuote = () => {
@@ -129,12 +147,8 @@ const CheckIfIndexIsInArray = (numberToCheck, arrayToCheck) => {
 }
 
 const SetRandomBackgroundColor = () => {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  container.style.backgroundColor = color;
+  const randNum = Math.floor(Math.random() * randomColors.length);
+  container.style.backgroundColor = randomColors[randNum];
 }
 
 const quoteButton = document.querySelector("#loadQuote").addEventListener("click", GenerateNewQuote);
